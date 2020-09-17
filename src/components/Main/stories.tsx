6ed87d2d104b7b62c6d16e3a60e8e58d885785ef
Maicon Domingues <maikonbdomingues@gmail.com>
2020-09-17 07:43:23 -0300

    impr: update version storybook and stories configurations

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,18 +1,19 @@
-import { withKnobs, text } from '@storybook/addon-knobs'
+import { Story, Meta } from '@storybook/react/types-6-0'
 import Main from '.'
 
 export default {
   title: 'Main',
   component: Main,
-  decorators: [withKnobs]
+  args: {
+    title: 'default title',
+    description: 'default description'
+  }
+} as Meta
+
+export const Basic: Story = (args) => <Main {...args} />
+Basic.args = {
+  title: '123',
+  description: 'asdasd11515'
 }
 
-export const Basic = () => (
-  <Main
-    title={text('Title', 'React Avançado')}
-    description={text(
-      'Description',
-      'TypeScript, ReactJS, NextJS e Styled Components'
-    )}
-  />
-)
+export const Default: Story = (args) => <Main {...args} />
